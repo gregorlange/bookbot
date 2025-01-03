5f9c60f684b7c45d37d09c5966d72119277c3f55
gregorlange <gregor.lange@posteo.de>
2025-01-03 18:32:30 +0100

    added character-counting function

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 def main():
     frankenstein = "books/frankenstein.txt"
-    read_book(frankenstein)
+    book_content = read_book(frankenstein)
+    count_words(book_content)
+    count_characters(book_content)
 
 def read_book(book_path):
     with open(book_path) as book:
         content = book.read()
         print(content)
-        count_words(content)
+        return content
 
 def count_words(book_content):
     words = book_content.split()
     word_count = len(words)
     print(f"{word_count} words counted in this text.")
+
+def count_characters(book_content):
+    book_string = book_content.lower()
+    characters = {}
+    for character in book_string:
+        if character in characters:
+            characters[character] += 1
+        else:
+            characters[character] = 1
+    print(characters)
 
 main()
