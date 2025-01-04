3d6440bef7407acf2e45e08c851f2df5f8cd59be
gregorlange <gregor.lange@posteo.de>
2025-01-04 11:18:45 +0100

    added character count to report

--- main.py
+++ main.py
@@ -28,5 +28,13 @@ def count_characters(book_content):
 def book_report(book_path, word_count, characters):
     print(f"--- Begin report of {book_path} ---")
     print(f"{word_count} words found in the document")
+    print("")
+    character_sort = []
+    for character in characters:
+        character_sort.append({"character": character, "count": characters[character]})
+    #character_sort.sort(reverse=True)
+    for dict in character_sort:
+        if dict["character"].isalpha():
+            print(f"The '{dict["character"]}' character was found {dict["count"]} times")
 
 main()
